d2c94c422cf1beb4e9a0ae2bab84d7897fcedc97
Anthony G <agithaka@live.com>
2023-01-17 13:02:49 +0300

    Mobile Mockup daisyUI

---
+++ components/MobileMockup.tsx
@@ -0,0 +1,12 @@
+function MobileMockup() {
+  return (
+    <div className="mockup-phone">
+      <div className="camera"></div>
+      <div className="display">
+        <div className="artboard artboard-demo phone-1">Hi.</div>
+      </div>
+    </div>
+  );
+}
+
+export default MobileMockup;
