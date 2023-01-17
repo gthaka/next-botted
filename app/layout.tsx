30179a4a25ad8a97810e030b280d16fe1ad8a491
Anthony G <agithaka@live.com>
2023-01-17 12:56:27 +0300

    main layout

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import "../styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head />
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
