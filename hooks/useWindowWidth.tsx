f3ef0fec3b01d6ab69cd0f19f892950bf4d86a87
Anthony G <agithaka@live.com>
2023-02-15 00:59:38 +0300

    auto detect window width hook

---
+++ hooks/useWindowWidth.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const useWindowWidth = () => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [setWidth]);
+
+  return width;
+};
+
+export default useWindowWidth;
