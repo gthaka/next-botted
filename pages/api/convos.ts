f084f7d1cedc165219d8325b0171761d4e7dad57
Anthony G <agithaka@live.com>
2023-02-15 02:35:00 +0300

    refactor: Convos API - collate and shorten code

--- pages/api/convos.ts
+++ pages/api/convos.ts
@@ -3,28 +3,17 @@ import axios from 'axios';
 const API_URL = `${process.env.NEXT_PUBLIC_OPENAI_API_URL}`;
 const API_KEY = process.env.OPENAI_API_KEY;
 
-export default async function handler(req, res) {
+export default async function handler(req: { body: { params: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { input?: any; aiResponse?: any; error?: any; }): void; new(): any; }; }; }) {
   try {
-    const { input } = req.body;
+    const { params } = req.body;
     // Make request to OpenAI API
-    const response = await axios.post(API_URL,
-      {
-        model: "text-davinci-003",
-        prompt: `Q:${input}?`,
-        temperature: 0,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        // stop: ["\n"],
-      }
-      , {
-        headers: { 'Authorization': `Bearer ${API_KEY}` }
-      });
+    const response = await axios.post(API_URL, params, {
+      headers: { 'Authorization': `Bearer ${API_KEY}` }
+    });
     const aiResponse = (response.data.choices[0].text).replace('A:', '');
 
-    res.status(200).json({ input, aiResponse });
-  } catch (error) {
+    res.status(200).json({ aiResponse });
+  } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
 }
