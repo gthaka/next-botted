9131c6130dbc3874540682f1e71d7663b15b65f7
Anthony G <agithaka@live.com>
2023-02-15 02:38:36 +0300

    refactor: mobile mockup - change image

--- components/chat/BubbleIn.tsx
+++ components/chat/BubbleIn.tsx
@@ -3,7 +3,7 @@ export default function BubbleIn({ children }: { children: React.ReactNode }) {
     <div className="chat chat-start w-full ml-2">
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img src="https://placeimg.com/192/192/people" />
+          <img src="https://cdn.dribbble.com/users/281679/screenshots/14897126/media/f52c47307ac2daa0c727b1840c41d5ab.png?compress=1&resize=192x192" />
         </div>
       </div>
       <div className="chat-bubble">{children}</div>
