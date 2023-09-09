a284a4d68a3c407cbc1de865c76b375110bef8ec
Anthony G <agithaka@live.com>
2023-09-09 19:27:51 +0000

    replaced with - https://picsum.photos/640/360

--- components/chat/BubbleOut.tsx
+++ components/chat/BubbleOut.tsx
@@ -3,7 +3,7 @@ export default function BubbleIn({ children }: { children: React.ReactNode }) {
     <div className="chat chat-end  w-full mr-2">
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img src="https://placeimg.com/192/192/people" />
+          <img src="https://picsum.photos/640/360" />
         </div>
       </div>
       <div className="chat-bubble">{children}</div>
